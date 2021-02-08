62c32e49b1cb4cb0771f3b1807e392300ecedb09
Unknown <skrzynska.konstancja@gmail.com>
2021-02-08 22:16:15 +0100

    internal linking

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
-    <h1>Blue bird Home Page</h1>
+    <h1>Bird's Social App Home Page</h1>
+    <div class="users-list">
+      <router-link v-for="user in users" :to="{ name: 'UserProfile', params: {userId: user.id} }" :key="user.id">
+        {{ user.username }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-
+import {users} from "../assets/users";
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    return {
+      users
+    }
+  }
 }
 </script>
+<style scoped lang="scss">
+.users-list{
+  display:flex;
+  flex-direction: column;
+}
+</style>
